9213247e059b6c9fe0e0d68acaa4c938d2e5f101
CristianGongora <cristiangongora17@gmail.com>
2026-01-16 19:29:17 -0500

    Conexión a drive

--- js/config.js
+++ js/config.js
@@ -2,9 +2,9 @@
 // Renombra config.example.js a config.js y pon tus datos reales aquí
 
 export const CONFIG = {
-    CLIENT_ID: '',
-    API_KEY: '',
-    FOLDER_ID: '',
+    CLIENT_ID: '85358053038-sniie7u22p854210e5u1vub1lm58sn5i.apps.googleusercontent.com',
+    API_KEY: 'AIzaSyBlRhmdKvaFIFjHg247VJoA4L2muzK1mDE',
+    FOLDER_ID: '1ahVICCgSReaIvjkkQ9Gg5HGQ0biUmnfx',
     SCOPES: 'https://www.googleapis.com/auth/drive.file',
     // Número para recibir pedidos (Formato internacional sin + ni espacios, ej: 573001234567)
     WHATSAPP_PHONE: '+573157690773'
