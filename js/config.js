6546f34eed355d0a87e4d3e9cd7fea37a332abb5
CristianGongora <cristiangongora17@gmail.com>
2026-01-17 10:57:12 -0500

    nuevas credenciales de api

--- js/config.js
+++ js/config.js
@@ -2,8 +2,8 @@
 // Renombra config.example.js a config.js y pon tus datos reales aquí
 
 export const CONFIG = {
-    CLIENT_ID: '85358053038-sniie7u22p854210e5u1vub1lm58sn5i.apps.googleusercontent.com',
-    API_KEY: 'AIzaSyBlRhmdKvaFIFjHg247VJoA4L2muzK1mDE',
+    CLIENT_ID: '1074421133778-tr9o7pgl2rc2bl752cc50nv23ldj4t39.apps.googleusercontent.com',
+    API_KEY: 'AIzaSyCRoCV6MIW_8vFvXPRJLfRtgC_nEFVeECU',
     FOLDER_ID: '1ahVICCgSReaIvjkkQ9Gg5HGQ0biUmnfx',
     SCOPES: 'https://www.googleapis.com/auth/drive.file',
     // Número para recibir pedidos (Formato internacional sin + ni espacios, ej: 573001234567)
